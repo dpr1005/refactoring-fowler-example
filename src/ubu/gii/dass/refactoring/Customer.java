9ac63cad6630526e4eb1064b1c9f61a3622cb8d4
Daniel Puente <dpr1005@alu.ubu.es>
2022-05-25 23:08:04 +0200

    Extracted amountFor. Closes #1

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -1,26 +1,27 @@
 package ubu.gii.dass.refactoring;
-
-/**
-* Tema  Refactorizaciones 
-*
-* Ejemplo de aplicaci�n de refactorizaciones. Actualizado para colecciones gen�ricas de java 1.5
-*
-* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos L�pez</A>
-* @version 1.1
-* @see java.io.File
-*
-*/
 import java.util.*;
 
+
+/**
+ * Tema  Refactorizaciones
+ *
+ * Ejemplo de aplicaci�n de refactorizaciones. Actualizado para colecciones gen�ricas de java 1.5
+ *
+ * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos L�pez</A>
+ * @version 1.1
+ * @see java.io.File
+ *
+ *
+ */
 public class Customer {
-	private String _name;
-	private List<Rental> _rentals;
+	private final String _name;
+	private final List<Rental> _rentals;
 
 	public Customer(String name) {
 		_name = name;
-		_rentals = new ArrayList<Rental>();
+		_rentals = new ArrayList<>();
 
-	};
+	}
 
 	public void addRental(Rental arg) {
 		_rentals.add(arg);
@@ -28,48 +29,51 @@ public class Customer {
 
 	public String getName() {
 		return _name;
-	};
+	}
 
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = _rentals.iterator();
-		String result = "Rental Record for " + getName() + "\n";
+		StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 		while (rentals.hasNext()) {
-			double thisAmount = 0;
 			Rental each = rentals.next();
 			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
-			
+			double thisAmount = amountFor(each);
+
 			// add frequent renter points
 			frequentRenterPoints++;
-			// add bonus for a two day new release rental
+			// add bonus for a two-day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
 					&& each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result.append("\t").append(each.getMovie().getTitle()).append("\t").append(thisAmount).append("\n");
 			totalAmount += thisAmount;
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
-		return result;
+		result.append("Amount owed is ").append(totalAmount).append("\n");
+		result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+		return result.toString();
+	}
+
+	private double amountFor(Rental each) {
+		double thisAmount = 0;
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	}
 }
